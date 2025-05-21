cdfef69b18f3bc7e308e45ce16a8a231d73c5c16
gyokuro06 <gyokuro06@gmail.com>
2025-05-21 18:18:28 +0900

    format stdout

--- build_llm_from_scratch/main.py
+++ build_llm_from_scratch/main.py
@@ -61,12 +61,12 @@ if __name__ == "__main__":
     print("Input shape:\n", inputs.shape)
 
     token_embeddings = token_embedding_layer(inputs)
-    print(token_embeddings.shape)
+    print("\nToken embeddings shape:\n", token_embeddings.shape)
 
     context_length = max_length
     pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
     pos_embeddings = pos_embedding_layer(torch.arange(context_length))
-    print(pos_embeddings.shape)
+    print("\nPos embeddings shape:\n", pos_embeddings.shape)
 
     input_embeddings = token_embeddings + pos_embeddings
-    print(input_embeddings.shape)
+    print("\nInput embeddings shape:\n", input_embeddings.shape)
